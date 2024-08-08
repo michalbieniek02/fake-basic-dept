807d170c7c9692cae3c515f7f76c211f03bf2d5e
Michał Bieniek <michalbieniek02@gmail.com>
2024-08-08 15:50:36 +0200

    all done besides animations and drag scroll

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        customGray: '#252422',
+      },
+    },
   },
   plugins: [],
 }
